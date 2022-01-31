debc29a2c53833aa7c86a3b40164184abd5a00d9
Christopher Ross Gunnels <92119195+Crgunnels@users.noreply.github.com>
2022-01-31 15:51:37 -0500

    Add files via upload
    C# Files from current project.

---
+++ LocationTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain
+{
+	public class LocationTypes
+	{
+		public int Id { get; set; }
+		public string LocationType { get; set; }
+	}
+}
