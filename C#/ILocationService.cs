4c392efe715471f4558c687eb24b24a8d6169515
Christopher Ross Gunnels <92119195+Crgunnels@users.noreply.github.com>
2022-01-31 16:46:11 -0500

    Add files via upload

---
+++ C#/ILocationService.cs
@@ -0,0 +1,32 @@
+﻿using Sabio.Models;
+using Sabio.Models.Domain;
+using Sabio.Models.Requests;
+using Sabio.Models.Requests.Locations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface ILocationService
+    {
+        int Add(LocationAddRequest model, int userId);
+
+        Location Get(int id);
+
+        Paged<Location> GetLocationsByUserId(int userId, int pageIndex = 0, int pageSize = 100);
+
+        void Update(LocationUpdateRequest model, int userId);
+
+        void Delete(int id);
+
+        Paged<Location> Pagination(int pageIndex, int pageSize);
+
+        List<State> GetStatesByCountryId(int countryId);
+
+        List<LocationTypes> GetLocationTypes();
+
+    }
+}
